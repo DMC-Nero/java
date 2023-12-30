33d885c6e3a184b8829e247a638520b3d311d593
DMC-Nero <vemulanatraj@gmail.com>
2023-12-30 17:58:57 +0530

    add first git commit

---
+++ Var.java
@@ -0,0 +1,20 @@
+class Var {
+	public static void main(String[] args){
+	int price;
+	price = 150;  //variable assigning 
+	System.out.println( price );
+
+	price =200;  //variable assigning 
+	System.out.println( price );
+
+	int age=25; //variable initilization 
+	System.out.println( age );
+
+	age=age+1; //read and write operation 
+	System.out.println( age );
+
+	double pi=3.1415; //variable initilization 
+	System.out.println( pi );
+
+	}
+}
